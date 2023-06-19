5728f6af893008c9e94d0ed0ad20a9bd00d936e2
VitalyBoro <127157980+VitalyBoro@users.noreply.github.com>
2023-06-19 12:13:10 +0300

    Update Duck5.java

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,17 +1,5 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
-
-@Component
-public class Duck5 {
-
-    @Override
-    public String toString() {
-        return ", в утке яйцо " + "";
-    }
-}
-package koschei.models;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
